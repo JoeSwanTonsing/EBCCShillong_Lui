811b006502a3eb55db22c3f84c0a6c834d29d454
Suan Tonsing <suanasdf@gmail.com>
2020-11-24 22:01:19 +0530

    minor changes to status bar and routes and route names

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Button, View} from 'react-native';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 
-//Navigation Stacks
-import HomeStack from './src/navigation/HomeStack';
+//my components & Screens
+import DrawerContent from './src/navigation/DrawerContent';
+import FeedsStack from './src/navigation/FeedsStack';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStack} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Drawer.Navigator
+            initialRouteName="Feeds"
+            drawerContent={(props) => <DrawerContent {...props} />}>
+            <Drawer.Screen name="Feeds" component={FeedsStack} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </>
   );
 }
