c6f8a7800a224eb88a783432da2938062ae5d26a
Suan Tonsing <suanasdf@gmail.com>
2020-11-24 19:51:45 +0530

    Basic Temporary HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, ScrollView, Text} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Hello HomeScreen</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
