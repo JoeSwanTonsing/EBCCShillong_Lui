67d197feb5e633fa508c561cca4626347c4d62aa
Suan Tonsing <suanasdf@gmail.com>
2020-11-25 14:15:13 +0530

    added routes for videos & photos

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 
-//my components & Screens
+//my components & nav_stacks
 import DrawerContent from './src/navigation/DrawerContent';
 import FeedsStack from './src/navigation/FeedsStack';
 import HomeStack from './src/navigation/HomeStack';
+import VideosStack from './src/navigation/VideosStack';
+import PhotosStack from './src/navigation/PhotosStack';
 
 const Drawer = createDrawerNavigator();
 
@@ -22,6 +24,8 @@ export default function App() {
             drawerContent={(props) => <DrawerContent {...props} />}>
             <Drawer.Screen name="Feeds" component={FeedsStack} />
             <Drawer.Screen name="Home" component={HomeStack} />
+            <Drawer.Screen name="Videos" component={VideosStack} />
+            <Drawer.Screen name="Photos" component={PhotosStack} />
           </Drawer.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
