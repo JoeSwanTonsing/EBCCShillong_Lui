0fee9619e70b5bc38b7a33b41e4b8001daaa1a16
Suan Tonsing <suanasdf@gmail.com>
2020-11-27 01:58:40 +0530

    ui improvement

--- src/screens/videos/VideosScreen.js
+++ src/screens/videos/VideosScreen.js
@@ -7,15 +7,81 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-export default function VideosScreen() {
+//https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=song&type=video&key=AIzaSyDqrTNLt0BchieTOXKozxk_80mHM210upE
+
+//My Components
+import Spinner from '../../components/Spinner';
+import Card from '../../components/cards/Card';
+
+export default function VideosScreen({navigation}) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [miniCardData, setMiniCard] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://joeswantonsing.github.io/ebccshg_data/api/youtube_videos.json',
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        console.log(data.items);
+        setMiniCard(data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function renderVideos() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          {miniCardData.map((video) => (
+            <TouchableOpacity
+              key={video.id.videoId}
+              onPress={() =>
+                alert('Open Video in Player with ID: ' + video.id.videoId)
+              }>
+              <Card
+                videoId={video.id.videoId}
+                title={video.snippet.title}
+                description={video.snippet.description}
+              />
+            </TouchableOpacity>
+          ))}
+
+          {/*<Card />
+          <Card />
+          <Card />
+          <Card />*/}
+        </View>
+      </ScrollView>
+    );
+  }
   return (
     <SafeAreaView style={{flex: 1}}>
-      <ScrollView>
-        <Text>Hello Videos Screen</Text>
-        <Text>Show Latest Videos in this Section</Text>
-        <Text>Show Playlists in this Section</Text>
-        <Text>Show All Videos in this Section</Text>
-      </ScrollView>
+      {isLoading ? (
+        <View style={styles.spinnerContainer}>
+          <Spinner
+            color="#264653"
+            spinnerText="Ngak Zek Lai Hamham Ni Mo . . ."
+          />
+        </View>
+      ) : (
+        renderVideos()
+      )}
     </SafeAreaView>
   );
 }
+
+const styles = {
+  container: {
+    marginTop: 15,
+  },
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
